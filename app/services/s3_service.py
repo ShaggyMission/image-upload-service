020fcdd76bc0e54148168f212314a5a5a667c23b
Lis Pacheco <lmpacheco@uce.edu.ec>
2025-06-21 15:03:45 -0500

    fix: load AWS credentials and bucket info from environment variables in S3 upload service

--- app/services/s3_service.py
+++ app/services/s3_service.py
@@ -1,8 +1,18 @@
+import os
 import boto3
 import uuid
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
+AWS_REGION = os.getenv("AWS_REGION")
+BUCKET_NAME = os.getenv("BUCKET_NAME") 
 
 s3 = boto3.client(
-    's3',
+    "s3",
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
     aws_session_token=AWS_SESSION_TOKEN,
