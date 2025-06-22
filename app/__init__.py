eb4e7ec7942d03bd2cf87124050a728ec1f47a27
Lis Pacheco <lmpacheco@uce.edu.ec>
2025-06-21 19:57:07 -0500

    feat(images): allow multiple image uploads to S3 via Flask endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flasgger import Swagger
+from flask_cors import CORS  # ✅ Importa CORS
 from .config import load_env_vars
 from .routes.image_routes import image_bp
 
@@ -7,7 +8,8 @@ def create_app():
     load_env_vars()
 
     app = Flask(__name__)
-    Swagger(app, template_file='docs/swagger.yml')  
+    CORS(app)
+    Swagger(app, template_file='docs/swagger.yml')
 
     app.register_blueprint(image_bp, url_prefix='/pets/images')
 
